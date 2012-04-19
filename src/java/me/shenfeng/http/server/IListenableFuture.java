4114b55bdfca05f79065cd92533e9dab63c6c054
feng <feng.shen@onycloud.com>
2012-04-19 21:29:57 +0800

    doc the async extension to the RING spec

--- src/java/me/shenfeng/http/server/IListenableFuture.java
+++ src/java/me/shenfeng/http/server/IListenableFuture.java
@@ -1,7 +1,24 @@
 package me.shenfeng.http.server;
 
+/**
+ * An async extension to the Ring SPEC.
+ * 
+ * The ring adapter will add an runnable, expect it to be called when things are
+ * ready, then it will call the get method to get the real response
+ * 
+ * @author feng
+ * 
+ */
 public interface IListenableFuture {
+    /**
+     * register a listener, should be called when things become ready
+     * 
+     * @param listener
+     */
     void addListener(Runnable listener);
 
+    /**
+     * @return ring SPEC response
+     */
     public Object get();
 }
