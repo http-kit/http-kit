7faa84df3df59329a05b842464a985cce1c4a2a9
feng <shenedu@gmail.com>
2013-08-17 13:39:25 +0800

    more robust websocket decoder, behave well even if client send one byte at a time

--- src/java/org/httpkit/server/WSDecoder.java
+++ src/java/org/httpkit/server/WSDecoder.java
@@ -15,7 +15,7 @@ public class WSDecoder {
     public static final byte OPCODE_PONG = 0xA;
 
     public enum State {
-        FRAME_START, MASKING_KEY, PAYLOAD, CORRUPT
+        FRAME_START, READ_LENGTH, READ_2_LENGTH, READ_8_LENGTH, MASKING_KEY, PAYLOAD, CORRUPT
     }
 
     private State state = State.FRAME_START;
@@ -28,6 +28,22 @@ public class WSDecoder {
     private boolean finalFlag;
     private int opcode = -1;
     private int framePayloadIndex; // masking per frame
+
+    // 8 bytes are enough
+    // protect against long/short/int are not fully received
+    private ByteBuffer tmpBuffer = ByteBuffer.allocate(8);
+
+    private boolean isAvailable(ByteBuffer src, int length) {
+        while (tmpBuffer.position() < length) {
+            if (src.hasRemaining()) {
+                tmpBuffer.put(src.get());
+            } else {
+                return false;
+            }
+        }
+        tmpBuffer.flip(); // for read
+        return true;
+    }
 
     public Frame decode(ByteBuffer buffer) throws ProtocolException {
         while (buffer.hasRemaining()) {
@@ -43,49 +59,69 @@ public class WSDecoder {
                                 + tmpOp);
                     }
                     opcode = tmpOp;
+                    state = State.READ_LENGTH;
+                    break;
+                case READ_LENGTH:
                     b = buffer.get(); // MASK, PAYLOAD LEN 1
                     boolean masked = (b & 0x80) != 0;
+                    if (!masked) {
+                        throw new ProtocolException("unmasked client to server frame");
+                    }
                     payloadLength = b & 0x7F;
-
                     if (payloadLength == 126) {
-                        payloadLength = buffer.getShort() & 0xFFFF;
+                        state = State.READ_2_LENGTH;
+                    } else if (payloadLength == 127) {
+                        state = State.READ_8_LENGTH;
+                    } else {
+                        state = State.MASKING_KEY;
+                    }
+                    break;
+                case READ_2_LENGTH:
+                    if (isAvailable(buffer, 2)) {
+                        payloadLength = tmpBuffer.getShort() & 0xFFFF;
+                        tmpBuffer.clear();
                         if (payloadLength < 126) {
                             throw new ProtocolException(
                                     "invalid data frame length (not using minimal length encoding)");
                         }
-                    } else if (payloadLength == 127) {
-                        long length = buffer.getLong();
+                        state = State.MASKING_KEY;
+                    }
+                    break;
+                case READ_8_LENGTH:
+                    if (isAvailable(buffer, 8)) {
+                        long length = tmpBuffer.getLong();
+                        tmpBuffer.clear();
                         // if negative, that too big, drop it.
                         if (length < 65536) {
                             throw new ProtocolException("invalid data frame length. max payload length 4M");
                         }
                         abortIfTooLarge(length);
                         payloadLength = (int) length;
+                        state = State.MASKING_KEY;
                     }
-
-                    if (content == null) {
-                        content = new byte[payloadLength];
-                    } else if (payloadLength > 0) {
-                        abortIfTooLarge(content.length + payloadLength);
-                    /*
-                     * TODO if an attacker sent many fragmented frames, only one
-                     * byte of data per frame, server end up reallocate many
-                     * times. may not be a problem
-                     */
-                        // resize
-                        content = Arrays.copyOf(content, content.length + payloadLength);
-                    }
-
-                    if (!masked) {
-                        throw new ProtocolException("unmasked client to server frame");
-                    }
-                    state = State.MASKING_KEY;
-                    framePayloadIndex = 0; // reset
-                    break;
+                    break; // wait for more data from TCP
                 case MASKING_KEY:
-                    maskingKey = buffer.getInt();
-                    state = State.PAYLOAD;
-                    // No break. since payloadLength can be 0
+                    if (isAvailable(buffer, 4)) {
+                        maskingKey = tmpBuffer.getInt();
+                        tmpBuffer.clear();
+                        if (content == null) {
+                            content = new byte[payloadLength];
+                        } else if (payloadLength > 0) {
+                            abortIfTooLarge(content.length + payloadLength);
+                            /*
+                             * TODO if an attacker sent many fragmented frames, only one
+                             * byte of data per frame, server end up reallocate many
+                             * times. may not be a problem
+                             */
+                            // resize
+                            content = Arrays.copyOf(content, content.length + payloadLength);
+                        }
+                        framePayloadIndex = 0; // reset
+                        state = State.PAYLOAD;
+                        // No break. since payloadLength can be 0
+                    } else {
+                        break; // wait for more data from TCP
+                    }
                 case PAYLOAD:
                     int read = Math.min(buffer.remaining(), payloadLength - payloadRead);
                     if (read > 0) {
@@ -101,7 +137,7 @@ public class WSDecoder {
                     }
                     framePayloadIndex += read;
 
-                    // all read
+                    // all read (this frame)
                     if (payloadRead == payloadLength) {
                         if (finalFlag) {
                             switch (opcode) {
