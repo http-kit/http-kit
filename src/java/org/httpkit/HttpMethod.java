69c816d5674bcf0877bf79dd1023a574caa9e549
Robbie Huffman <robbie.huffman@gmail.com>
2014-06-24 18:14:31 -0400

    Added methods required for CalDAV

--- src/java/org/httpkit/HttpMethod.java
+++ src/java/org/httpkit/HttpMethod.java
@@ -8,7 +8,10 @@ public enum HttpMethod {
 
     GET(intern("get")), HEAD(intern("head")), POST(intern("post")), PUT(intern("put")),
     DELETE(intern("delete")), TRACE(intern("trace")), OPTIONS(intern("options")),
-    CONNECT(intern("connect")), PATCH(intern("patch"));
+    CONNECT(intern("connect")), PATCH(intern("patch")), PROPFIND(intern("propfind")),
+	PROPPATCH(intern("proppatch")), LOCK(intern("lock")), UNLOCK(intern("unlock")),
+	REPORT(intern("report")), ACL(intern("acl")), MOVE(intern("move")), 
+	COPY(intern("copy"));
 
     public final Keyword KEY;
 
