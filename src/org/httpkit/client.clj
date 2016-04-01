9bb3cae99a40869e1167adb40a61fe13bbfc2d62
Jonathan Bristow <Jonathan_Bristow@intuit.com>
2016-04-01 06:59:12 -0700

    [#280 #281 #291] Client: allow https requests through proxy, tunneling (@jshaw86, @jbristow)
    Squashed:
      - Adding fix to allow https requests through http-proxy
      - Added CONNECT header for http endpoints, add tunnel option for proxies that do not support CONNECT
      - proxy-host, proxy-port api changes

--- src/org/httpkit/client.clj
+++ src/org/httpkit/client.clj
@@ -138,7 +138,7 @@
     :url :method :headers :timeout :query-params :form-params :as
     :client :body :basic-auth :user-agent :filter :worker-pool"
   [{:keys [client timeout filter worker-pool keepalive as follow-redirects max-redirects response
-           trace-redirects allow-unsafe-redirect-methods proxy]
+           trace-redirects allow-unsafe-redirect-methods proxy-host proxy-port tunnel?]
     :as opts
     :or {client @default-client
          timeout 60000
@@ -149,7 +149,9 @@
          response (promise)
          keepalive 120000
          as :auto
-         proxy nil}}
+         tunnel? false
+         proxy-host nil
+         proxy-port 3128}}
    & [callback]]
   (let [{:keys [url method headers body sslengine]} (coerce-req opts)
         deliver-resp #(deliver response ;; deliver the result
@@ -189,7 +191,7 @@
         listener (RespListener. handler filter worker-pool
                                 ;; only the 4 support now
                                 (case as :auto 1 :text 2 :stream 3 :byte-array 4))
-        cfg (RequestConfig. method headers body timeout keepalive proxy)]
+        cfg (RequestConfig. method headers body timeout keepalive proxy-host proxy-port tunnel?)]
     (.exec ^HttpClient client url cfg sslengine listener)
     response))
 
