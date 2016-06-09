8dba65d9356f4b5bfaa38b4f699927e666cd91e8
Peter Taoussanis <ptaoussanis@gmail.com>
2016-06-09 12:34:40 +0700

    [#293] Tests housekeeping

--- test/org/httpkit/num_connections.clj
+++ test/org/httpkit/num_connections.clj
@@ -3,24 +3,43 @@
              [org.httpkit.server :refer [run-server]]
              [org.httpkit.client :as client]))
 
-(defn h [p]
-  (fn [r]
-    (if (= (:uri r) "/wait")
-      {:status @p}
-      {:status 200})))
-
 (deftest one-connection
-  (let [p (promise)
-        c (org.httpkit.client.HttpClient. 1)
-        s (run-server (h p) {:port 4347})
-        r1 (client/get "http://localhost:4347/wait" {:client c})
-        r2 (client/get "http://localhost:4347" {:client c})]
+  (let [client  (org.httpkit.client.HttpClient. 1) ; Max 1 conn
+        p       (promise)
+        handler (fn [req]
+                  (if (= (:uri req) "/wait")
+                    {:status @p}
+                    {:status 200}))
+        server (run-server handler {:port 4347})
+        r1 (client/get "http://localhost:4347/wait" {:client client})
+        r2 (client/get "http://localhost:4347"      {:client client})]
+
     (try
+      (Thread/sleep 100)
       (is (not (realized? r1)))
-      (is (not (realized? r2)))
-      (deliver p 200)
+      (is (not (realized? r2))) ; Waiting till r1 completes
+      (deliver p 200) ; Complete r1
       (Thread/sleep 100)
       (is (realized? r1))
-      (Thread/sleep 100)
       (is (realized? r2))
-      (finally (s)))))
+      (finally (server)))))
+
+(deftest many-connections
+  (let [client  (org.httpkit.client.HttpClient.) ; Unlimited conns
+        p       (promise)
+        handler (fn [req]
+                  (if (= (:uri req) "/wait")
+                    {:status @p}
+                    {:status 200}))
+        server (run-server handler {:port 4347})
+        r1 (client/get "http://localhost:4347/wait" {:client client})
+        r2 (client/get "http://localhost:4347"      {:client client})]
+
+    (try
+      (Thread/sleep 100)
+      (is (not (realized? r1)))
+      (is      (realized? r2)) ; Not waiting till r1 completes
+      (deliver p 200) ; Complete r1
+      (Thread/sleep 100)
+      (is (realized? r1))
+      (finally (server)))))
