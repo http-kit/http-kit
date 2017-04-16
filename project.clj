7b6dfec235bbd54dba12db66ed7922fa13c306e1
Peter Taoussanis <ptaoussanis@gmail.com>
2017-04-16 14:46:23 +0200

    Bump deps

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
   [[lein-swank   "1.4.5"]
    [lein-pprint  "1.1.2"]
    [lein-ancient "0.6.10"]
-   [lein-codox   "0.9.5"]]
+   [lein-codox   "0.10.3"]]
 
   :jvm-opts
   ["-Dclojure.compiler.disable-locals-clearing=true"
@@ -33,7 +33,7 @@
 
   :profiles
   {:test {:java-source-paths ["test/java" "src/java"]
-          :dependencies [[ring/ring-defaults "0.2.1"]
+          :dependencies [[ring/ring-defaults "0.2.3"]
                          [ring-request-proxy "0.1.4"]
                          [ring-basic-authentication "1.0.5"]
                          [org.clojure/data.codec "0.1.0"]]}
@@ -41,12 +41,12 @@
           [[org.clojure/clojure "1.8.0"]
            [junit/junit "4.12"]
            [org.clojure/tools.logging "0.3.1"]
-           [ch.qos.logback/logback-classic "1.1.7"]
+           [ch.qos.logback/logback-classic "1.2.3"]
            [clj-http "2.1.0"] ; TODO Update (breaking?)
            [io.netty/netty "3.6.5.Final"] ; TODO Update (breaking)
            [org.clojure/data.json "0.2.6"]
            [http.async.client "0.5.2"] ; TODO Update (breaking)
-           [compojure "1.5.1"]
+           [compojure "1.5.2"]
            [org.clojure/tools.cli "0.3.3"] ; TODO Update (breaking)
-           [ring/ring-jetty-adapter "1.5.0"]
-           [ring/ring-core "1.5.0"]]}})
+           [ring/ring-jetty-adapter "1.5.1"]
+           [ring/ring-core "1.5.1"]]}})
