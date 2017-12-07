e0505e54ffc67437ac7c2a78a90925914cc6a761
Raghu Kaippully <raghu.kaippully@concur.com>
2017-12-07 18:43:01 +0530

    Error while processing chunked responses split between CRLF characters
    Fixes issue #3. Handle cases where the bytes read may not contain both CR and
    LF characters in the same packet.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject http-kit "2.3.0-alpha4"
+(defproject org.clojars.rkaippully/http-kit "2.3.0-alpha4-patch1"
   :author "Feng Shen (@shenfeng)"
   :description "High-performance event-driven HTTP client/server for Clojure"
   :url "http://http-kit.org/"
--- src/java/org/httpkit/client/Decoder.java
+++ src/java/org/httpkit/client/Decoder.java
@@ -109,12 +109,10 @@ public class Decoder {
                     readBody(buffer, READ_CHUNK_DELIMITER);
                     break;
                 case READ_CHUNK_FOOTER:
-                    readEmptyLine(buffer);
-                    state = ALL_READ;
+                    readEmptyLine(buffer, ALL_READ);
                     break;
                 case READ_CHUNK_DELIMITER:
-                    readEmptyLine(buffer);
-                    state = READ_CHUNK_SIZE;
+                    readEmptyLine(buffer, READ_CHUNK_SIZE);
                     break;
                 case READ_VARIABLE_LENGTH_CONTENT:
                     readBody(buffer, null);
@@ -137,14 +135,18 @@ public class Decoder {
         }
     }
 
-    void readEmptyLine(ByteBuffer buffer) {
-        byte b = buffer.get();
-        if (b == CR && buffer.hasRemaining()) {
-            buffer.get(); // should be LF
+    private void readEmptyLine(ByteBuffer buffer, State nextState) throws ProtocolException, LineTooLargeException {
+        String line = lineReader.readLine(buffer);
+        if (line != null) {
+            if (line.isEmpty()) {
+                state = nextState;
+            } else {
+                throw new ProtocolException("Expected an empty line, but found " + line);
+            }
         }
     }
 
-    private void readHeaders(ByteBuffer buffer) throws LineTooLargeException, AbortException {
+    private void readHeaders(ByteBuffer buffer) throws LineTooLargeException, AbortException, ProtocolException {
         String line = lineReader.readLine(buffer);
         while (line != null && !line.isEmpty()) {
             HttpUtils.splitAndAddHeader(line, headers);
