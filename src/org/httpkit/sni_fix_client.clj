eab070d050c57530854be6430166b6052d30a516
Francesco Pischedda <francesco.pischedda@redpineapplemedia.com>
2020-01-02 14:20:18 +0100

    provide request helpers like http-kit.client
    these helpers will automatically add the fixed client if a :client is
    not specified in the options map

--- src/org/httpkit/sni_fix_client.clj
+++ src/org/httpkit/sni_fix_client.clj
@@ -22,6 +22,9 @@
   []
   (http-client/make-client {:ssl-configurer ssl-configurer}))
 
+;;; "Get the default client. Normally, you only need one client per application. You can config parameter per request basic"
+(defonce default-sni-fixed-client (delay (HttpClient.)))
+
 (comment
 
   (def client (http-client/make-client))
@@ -30,3 +33,38 @@
                          :url "https://www.google.com/"
                          :client client})
   )
+
+(defn request
+  "Wraps http-kit.client/request to provide a sni-fixed client
+  if a client key is not provided by the caller"
+  [options & [callback]]
+  ;; client defaults to default-sni-fixed-client if not specified
+  (let [client (get options :client @default-sni-fixed-client)]
+    (http-client/request (assoc options :client client) callback)))
+
+;; replicate http-kit.client macro using the request function wrapper
+(defmacro ^:private defreq [method]
+  `(defn ~method
+     ~(str "Issues an async HTTP " (str/upper-case method) " request. "
+           "See `request` for details.")
+     ~'{:arglists '([url & [opts callback]] [url & [callback]])}
+     ~'[url & [s1 s2]]
+     (if (or (instance? clojure.lang.MultiFn ~'s1) (fn? ~'s1) (keyword? ~'s1))
+       (request {:url ~'url :method ~(keyword method)} ~'s1)
+       (request (merge ~'s1 {:url ~'url :method ~(keyword method)}) ~'s2))))
+
+(defreq get)
+(defreq delete)
+(defreq head)
+(defreq post)
+(defreq put)
+(defreq options)
+(defreq patch)
+(defreq propfind)
+(defreq proppatch)
+(defreq lock)
+(defreq unlock)
+(defreq report)
+(defreq acl)
+(defreq copy)
+(defreq move)
