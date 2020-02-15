4708cf093a33988c9c1cc25c9c6d7d3b62fcf78c
Dimitrios Piliouras <jimpil1985@gmail.com>
2020-02-15 12:58:47 +0000

    tests passing on jdk11/12

--- src/org/httpkit/sni_client.clj
+++ src/org/httpkit/sni_client.clj
@@ -2,20 +2,16 @@
   "Provides an SNI-capable SSL configurer and client, Ref. #335.
   In a separate namespace from `org.httpkit.client` so that
   http-kit can retain backwards-compatibility with JVM < 8."
+  (:require [clojure.string :as str])
   (:import [java.net URI]
            [javax.net.ssl SNIHostName SSLEngine SSLParameters]))
 
-(defn ssl-configurer
-  "SNI-capable SSL configurer.
-  May be used as an argument to `org.httpkit.client/make-client`:
-    (make-client :ssl-configurer (ssl-configurer))"
-  [^SSLEngine ssl-engine ^URI uri]
-  (let [host-name  (SNIHostName. (.getHost uri))
-        ssl-params (doto (.getSSLParameters ssl-engine)
-                     (.setServerNames [host-name]))]
-    (doto ssl-engine
-      (.setUseClientMode true) ; required for JVM 12/13 but not for JVM 8
-      (.setSSLParameters ssl-params))))
+(defonce jv ;; java major version as an int
+  (delay
+    (-> (System/getProperty "java.version")
+        (str/split #"\.")
+        first
+        Integer/parseInt)))
 
 (def ^:private sni? (partial = :sni))
 (def ^:private hv?  (partial = :hostname-verification))
@@ -39,13 +35,14 @@
          ;; java.lang.IllegalArgumentException:
          ;; Cannot change mode after SSL traffic has started
          ;; at sun.security.ssl.SSLEngineImpl.setUseClientMode
-         (not client-mode?)
+         (or (not client-mode?)
+             (>= (force jv) 11))
          (.setUseClientMode true))
        (.setSSLParameters ssl-params)))))
 
 
 
-(defonce
+(def
   ^{:doc "Like `org.httpkit.client/default-client`, but provides SNI support using `ssl-configurer`"}
   default-client
   (delay
--- test/org/httpkit/client_https_test.clj
+++ test/org/httpkit/client_https_test.clj
@@ -7,19 +7,26 @@
 
 (deftest client-https-tests
   (testing "`sni/default-client` behaves similarly to `URL.openStream()`"
-    (let [sslengine (.createSSLEngine (SSLContext/getDefault))
-          https-client (force sni/default-client)
+    (let [https-client (force sni/default-client)
           url1 "https://wrong.host.badssl.com"
           url2 "https://self-signed.badssl.com"
           url3 "https://untrusted-root.badssl.com"]
 
+      (is (nil?
+            (:error
+              @(http/request
+                 {:client  https-client
+                  :sslengine (.createSSLEngine (SSLContext/getDefault))
+                  :keepalive -1
+                  :url "https://www.bbc.co.uk"}))))
+
       (let [exception (:error
                         @(http/request
                            {:client https-client
-                            :sslengine sslengine
+                            :sslengine (.createSSLEngine (SSLContext/getDefault))
                             :keepalive -1
                             :url url1}))]
-                ;; Java 9 and above throws proper SSLHandshakeException
+                ;; Java 12 and above throws proper SSLHandshakeException
         (is (or (instance? SSLHandshakeException exception)
                 ;; Java 8 throws RuntimeException at sun.security.ssl.Handshaker
                 (instance? RuntimeException exception))))
@@ -28,7 +35,7 @@
                      (:error
                        @(http/request
                           {:client  https-client
-                           :sslengine sslengine
+                           :sslengine (.createSSLEngine (SSLContext/getDefault))
                            :keepalive -1
                            :url url2}))))
 
@@ -36,9 +43,18 @@
                      (:error
                        @(http/request
                           {:client  https-client
-                           :sslengine sslengine
+                           :sslengine (.createSSLEngine (SSLContext/getDefault))
                            :keepalive -1
                            :url url3}))))
+
+      (is (nil?
+              (:error
+                @(http/request
+                   {:client  https-client
+                    :sslengine (.createSSLEngine (SSLContext/getDefault))
+                    :keepalive -1
+                    :url "https://github.com"}))
+              ))
 
       )
     )
