13e38b8e68b0cd05803a0a7e393883970d8ac204
Greg Look <greg@mvxcvi.com>
2021-12-17 22:54:47 -0800

    [#485] [Client] [New] Add HTTP LIST method (@greglook)

--- src/java/org/httpkit/HttpMethod.java
+++ src/java/org/httpkit/HttpMethod.java
@@ -9,9 +9,9 @@ public enum HttpMethod {
     GET(intern("get")), HEAD(intern("head")), POST(intern("post")), PUT(intern("put")),
     DELETE(intern("delete")), TRACE(intern("trace")), OPTIONS(intern("options")),
     CONNECT(intern("connect")), PATCH(intern("patch")), PROPFIND(intern("propfind")),
-	PROPPATCH(intern("proppatch")), LOCK(intern("lock")), UNLOCK(intern("unlock")),
-	REPORT(intern("report")), ACL(intern("acl")), MOVE(intern("move")), 
-	COPY(intern("copy")), MKCOL(intern("mkcol"));
+    PROPPATCH(intern("proppatch")), LOCK(intern("lock")), UNLOCK(intern("unlock")),
+    REPORT(intern("report")), ACL(intern("acl")), MOVE(intern("move")),
+    COPY(intern("copy")), MKCOL(intern("mkcol")), LIST(intern("list"));
 
     public final Keyword KEY;
 
