eafe5489e7e41c0eb9dd79b17ecc503430420cb5
tweeks-reify <69803975+tweeks-reify@users.noreply.github.com>
2022-07-29 19:01:55 -0400

    Update HttpServer.java

--- src/java/org/httpkit/server/HttpServer.java
+++ src/java/org/httpkit/server/HttpServer.java
@@ -387,7 +387,7 @@ public class HttpServer implements Runnable {
                 // jvm can catch any exception, including OOM
             } catch (Throwable e) { // catch any exception(including OOM), print it
                 status.set(Status.STOPPED);
-                errorLogger.log("http server loop error, investigate this error:", e);
+                errorLogger.log("http server loop error, see stack trace for details", e);
                 eventLogger.log(eventNames.serverLoopError);
             }
         }
