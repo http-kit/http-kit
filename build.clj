e2d71039ea2617e789a08606a0c404c41367dca8
Michiel Borkent <michielborkent@gmail.com>
2023-03-03 23:44:52 +0100

    [new] [Build] [#509] [#511] Add native-image test to CI (@borkdude)
    This commit adds:
    
    * a test-native project in which httpkit client + server is tests with GraalVM native-image
    * a deps.edn which is used by test-native but can be used by any deps.edn user
    * a Github Action yml for running test-native in linux, macOS and Windows
    * reflection fixes in httpkit server which were found during the tests

---
+++ build.clj
@@ -0,0 +1,7 @@
+(ns build
+  (:require [babashka.process :refer [shell]]))
+
+(set! *warn-on-reflection* true)
+
+(defn lein-javac [_]
+  (shell "lein javac"))
---
+++ test-native/build.clj
@@ -0,0 +1,25 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'my/native-test)
+
+(def version "0.0.1")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis basis
+           :main 'native.test})
+  (println "Created uberjar:" uber-file))
