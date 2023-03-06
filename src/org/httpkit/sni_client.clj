05c5ac9a398f07b7d4c20161920e4912ebde4a37
Peter Taoussanis <peter@taoensso.com>
2023-03-06 15:12:10 +0100

    [new] [Client] [#393] Use SNI client by default for Java >= 8 (@aiba)

--- src/org/httpkit/sni_client.clj
+++ src/org/httpkit/sni_client.clj
@@ -1,9 +1,11 @@
 (ns org.httpkit.sni-client
   "Provides an SNI-capable SSL configurer and client, Ref. #335.
-  In a separate namespace from `org.httpkit.client` so that
-  http-kit can retain backwards-compatibility with JVM < 8.
 
-  NB hostname verification currently requires Java version >= 11."
+  Needs Java >= 8, hostname verification needs Java >= 11.
+
+  Originally in a separate namespace from `org.httpkit.client` to
+  retain backwards-compatibility with Java < 8."
+
   (:require
    [org.httpkit.client]
    [org.httpkit.utils :as utils])
@@ -36,7 +38,8 @@
        (.setSSLParameters ssl-params)))))
 
 (defonce
-  ^{:doc "Like `org.httpkit.client/default-client`, but provides SNI support using `ssl-configurer`."}
+  ^{:deprecated "v2.7"
+    :doc "Like `org.httpkit.client/default-client`, but provides SNI support using `ssl-configurer`."}
   default-client
   (delay
     (org.httpkit.client/make-client
