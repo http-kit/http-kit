http-kit/http-kit

commit e28e4c6e77835d2768a117f12a4ab3e3a42283b2

[#383] Set default SSLContext to use default TrustManagers (@ryfow)
Frankly, I'm not sure what changed in Java 11 such that the
default SSLContext doesn't trust google.com's cert, but
initializing our own SSLContext appears to fix it.

I added a test that connects to miscellaneous public https
sites in hopes of catching these sorts of problems automatically.

src/java/org/httpkit/client/HttpClient.java
@@ -16,6 +16,7 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.security.NoSuchAlgorithmException;
+import java.security.KeyManagementException;
 import java.util.Iterator;
 import java.util.Queue;
 import java.util.Set;
@@ -36,8 +37,9 @@ public class HttpClient implements Runnable {
 
     static {
         try {
-            DEFAULT_CONTEXT = SSLContext.getDefault();
-        } catch (NoSuchAlgorithmException e) {
+            DEFAULT_CONTEXT = SSLContext.getInstance("TLS");
+            DEFAULT_CONTEXT.init(null, TrustManagerFactory.getTrustManagers() ,null);
+        } catch (Exception e) {
             throw new Error("Failed to initialize SSLContext", e);
         }
     }

test/org/httpkit/client_test.clj
@@ -292,6 +292,14 @@
                                      (assoc (rand-keep-alive) :insecure? true))
                           :body count)))))))
 
+(deftest test-misc-https-certs
+  ;; Check to make sure an https connection works using the default trust store.
+  (is (contains? @(http/get "https://status.github.com/api/status.json") :status))
+  (is (contains? @(http/get "https://google.com") :status))
+  (is (contains? @(http/get "https://apple.com") :status))
+  (is (contains? @(http/get "https://microsoft.com") :status))
+  (is (contains? @(http/get "https://letsencrypt.org") :status)))
+
 ;; https://github.com/http-kit/http-kit/issues/54
 (deftest test-nested-param
   (let [url "http://localhost:4347/nested-param"

commit 3493ec41eb13e595a24cabf59753950127439d07

[fix] [client] fix handling of some bad ssl certificates
Broke in e28e4c6e77835d2768a117f12a4ab3e3a42283b2 (between v2.3.0 and v2.4.0-alpha1)

src/java/org/httpkit/client/HttpClient.java
@@ -89,9 +89,7 @@ public class HttpClient implements Runnable {
     public static SSLContext getDefaultContext() {
         if(defaultContext==null) {
             try {
-                SSLContext sslContext = SSLContext.getInstance("TLS");
-                sslContext.init(null, TrustManagerFactory.getTrustManagers() ,null);
-                defaultContext = sslContext;
+                defaultContext = SSLContext.getDefault();
             } catch (Exception e) {
                 throw new Error("Failed to initialize SSLContext", e);
             }

test/org/httpkit/client_test.clj
@@ -360,6 +360,20 @@
   (is (contains? @(hkc/get "https://microsoft.com")   :status))
   (is (contains? @(hkc/get "https://letsencrypt.org") :status)))
 
+(deftest test-bad-ssl-certs
+  (doseq [url ["https://expired.badssl.com/"
+               "https://wrong.host.badssl.com/"
+               "https://self-signed.badssl.com/"
+               "https://untrusted-root.badssl.com/"
+               ;; The revoked cert is expired: [Certificate https://revoked.badssl.com/ has expired · Issue #515 · chromium/badssl.com](https://github.com/chromium/badssl.com/issues/515)
+               ;; "https://revoked.badssl.com/"
+               ]]
+    (let [resp @(hkc/get url)]
+      (is (instance? javax.net.ssl.SSLHandshakeException (:error resp))
+          (str "HTTP GET to " url " did not throw an exception: "
+               (pr-str resp)))
+      (is (contains? @(hkc/get url {:insecure? true}) :status)))))
+
 (deftest test-multiple-https-calls-with-same-engine
   (let [opts {:client hkc/legacy-client
               :sslengine (ClientSslEngineFactory/trustAnybody)}]